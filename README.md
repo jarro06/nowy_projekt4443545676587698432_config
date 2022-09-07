54c72750a2b22ae396dcf671c2102e2b2bd1cbaa
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-09-07 12:44:09 +0200

    Add ADP toolchain configuration template

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# nowy_projekt4443545676587698432_config
-This is config for Mój super nowy projekt
+# ADP Toolchain Configuration
+This repository contains the personal configuration of the ADP toolchain.
+
+Please see the [**official documentation**](https://github.developer.allianz.io/AgileDeliveryPlatform/toolchain-documentation) for more information.
---
+++ jenkins/job-description/nowy_projekt4443545676587698432_view.groovy
@@ -0,0 +1,16 @@
+ 
+listView('nowy_projekt4443545676587698432 Jobs') {
+    description('nowy_projekt4443545676587698432 Jobs')
+    jobs {
+        regex('nowy_projekt4443545676587698432_.+')
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
